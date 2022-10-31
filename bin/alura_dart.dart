9afff62dee6e77b5b2d49d437fa9444e72173bfd
João Victor <jvdematos004@gmail.com>
2022-10-31 19:37:11 -0300

    Boolean

--- bin/alura_dart.dart
+++ bin/alura_dart.dart
@@ -3,7 +3,7 @@ import 'package:alura_dart/alura_dart.dart' as alura_dart;
 void main(List<String> arguments) {
   print('Hello world: ${alura_dart.calculate()}!');
 
-  int a = 20;
+  int a = 21;
   int b = 30;
 
   print(a + b);
@@ -13,4 +13,10 @@ void main(List<String> arguments) {
 
   double mil = 780e6;
   print(mil);
+
+  print(a == 20 ? 'verdade birl' : 'ksks');
+
+  bool boolean = true;
+
+  print(boolean);
 }
