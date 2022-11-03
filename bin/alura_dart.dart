479b7076bd053d197fa37d94ee1011e11c09a4bb
João Victor <jvdematos004@gmail.com>
2022-11-02 21:52:09 -0300

    if/else

--- bin/alura_dart.dart
+++ bin/alura_dart.dart
@@ -20,14 +20,15 @@ void main(List<String> arguments) {
 
   print(boolean);
 
-  int idade = 23;
+  int idade = 17;
   bool geek = true;
   String apelido = 'Dark';
-  String nome = 'João Victor Martins de matos';
-  double altura1 = 1.72;
+  const String nome = 'João Victor Martins de matos';
+  final double altura1 = 1.72; // valor n seje mudado
   String profissao = 'Java Developer';
 
-  List<dynamic> jhon = [ //dynamic pode usar qualquer tipo , mas não é recomendado usar muito
+  List<dynamic> jhon = [
+    //dynamic pode usar qualquer tipo , mas não é recomendado usar muito
     geek,
     idade,
     altura1,
@@ -41,16 +42,24 @@ void main(List<String> arguments) {
       'Eu tenho ${jhon[2]} metros de altura e tenho \n '
       '${jhon[1]} anos de idade');
 
-  String nome1 = 'a';
-  String nome2 = 'b';
-  String nome3 = 'c';
-  String nome4 = 'd';
-  String nome5 = 'e';
-
   List<String> listaNomes = ['ana', 'joao', 'natalia', 'alex', 'ricardo'];
   print(listaNomes.length);
   print(listaNomes[0]);
 
+  if (idade >= 18) {
+    print('Maior de idade');
+  } else {
+    print('Menor de idade');
+  }
+
+  bool maiorDeIdade;
+
+  if (idade >= 18) {
+    maiorDeIdade = true;
+  } else {
+    maiorDeIdade = false;
+  }
+  print(maiorDeIdade);
 
 
   //link útil sobre funções da lista
