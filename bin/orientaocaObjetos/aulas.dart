c76e8cef39d24eeb24149096998d1d11ca71c6db
João Victor <jvdematos004@gmail.com>
2022-11-27 13:06:30 -0300

    primeiros paço orientação a objeto

---
+++ bin/orientaocaObjetos/aulas.dart
@@ -0,0 +1,16 @@
+void main() {
+  print("Aula Orientação a objeto");
+  String nome = "laranja";
+  double peso = 102.2;
+  String cor = "verde e amarela";
+  String sabor = "Doce cítrica";
+  int diaDesdeColheita = 40;
+  bool isMadura;
+
+
+  if(diaDesdeColheita >=30) {
+    isMadura = true;
+  }else{
+    isMadura=false;
+  }
+}
